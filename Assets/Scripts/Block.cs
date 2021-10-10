ad2c182eec4b7908542e6f5bcc4cea380324fe37
Salvatore Ventrone <ventrosky@gmail.com>
2021-10-10 14:49:19 +0200

    creating/breaking block notes

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockColor{
+    Green,
+    Red
+}
+public class Block : MonoBehaviour {
+    public BlockColor color;
+
+    public GameObject brokenBlockLeft;
+    public GameObject brokenBlockRight;
+    public float brokenBlockForce;
+    public float brokenBlockTorque;
+    public float brokenBlockDestroyDelay;
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("SwordRed")){
+            if(color == BlockColor.Red){
+                // add score
+            } else {
+                // remove life
+            }
+            Hit();
+        } else if (other.CompareTag("SwordRed")){
+            if(color == BlockColor.Green){
+                // add score
+            } else {
+                // remove life
+            }
+            Hit();
+        }
+
+    }
+
+    void Hit(){
+        // enable broken pieces
+        brokenBlockLeft.SetActive(true);
+        brokenBlockRight.SetActive(true);
+
+        // remove as children
+        brokenBlockLeft.transform.parent = null;
+        brokenBlockRight.transform.parent = null;
+        
+        Rigidbody leftRig = brokenBlockLeft.GetComponent<Rigidbody>();
+        Rigidbody rigthRig = brokenBlockRight.GetComponent<Rigidbody>();
+
+        leftRig.AddForce(-transform.right * brokenBlockForce, ForceMode.Impulse);
+        rigthRig.AddForce(transform.right * brokenBlockForce, ForceMode.Impulse);
+
+        leftRig.AddTorque(-transform.forward * brokenBlockTorque, ForceMode.Impulse);
+        rigthRig.AddTorque(transform.forward * brokenBlockTorque, ForceMode.Impulse);
+
+        Destroy(brokenBlockLeft, brokenBlockDestroyDelay);
+        Destroy(brokenBlockRight, brokenBlockDestroyDelay);
+        Destroy(gameObject);
+    }
+}
